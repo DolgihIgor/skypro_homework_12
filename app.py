62f157f16a8b44343e2fda9b67fa90975c9802e7
Igor Dolgikh <87515633+DolgihIgor@users.noreply.github.com>
2023-03-29 22:38:55 +0700

    Init commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, render_template, send_from_directory
+# from functions import ...
+
+POST_PATH = "posts.json"
+UPLOAD_FOLDER = "uploads/images"
+
+from main.main import main_blueprint
+
+app = Flask(__name__)
+
+app.register_blueprint(main_blueprint)
+# @app.route("/")
+# def page_index():
+#     pass
+#
+#
+# @app.route("/list")
+# def page_tag():
+#     pass
+#
+#
+# @app.route("/post", methods=["GET", "POST"])
+# def page_post_form():
+#     pass
+#
+#
+# @app.route("/post", methods=["POST"])
+# def page_post_upload():
+#     pass
+#
+#
+# @app.route("/uploads/<path:path>")
+# def static_dir(path):
+#     return send_from_directory("uploads", path)
+
+
+app.run(debug=True)
+
